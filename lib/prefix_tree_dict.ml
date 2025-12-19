6919d6feed6b5be8e2d2f02a20558bd7dd6489f8
aeshabb <belalesa725@gmail.com>
2025-12-19 20:13:53 +0300

    ci added

--- lib/prefix_tree_dict.ml
+++ lib/prefix_tree_dict.ml
@@ -2,30 +2,21 @@
    Ключи - строки, значения - полиморфные.
    Структура неизменяемая и является моноидом. *)
 
-module CharMap = Map.Make(Char)
+module CharMap = Map.Make (Char)
 
 (* Узел префиксного дерева *)
-type 'a t = {
-  value: 'a option;
-  children: 'a t CharMap.t;
-}
+type 'a t = { value : 'a option; children : 'a t CharMap.t }
 
 (* Пустой словарь - нейтральный элемент моноида *)
-let empty = {
-  value = None;
-  children = CharMap.empty;
-}
-
-let is_empty t =
-  Option.is_none t.value && CharMap.is_empty t.children
+let empty = { value = None; children = CharMap.empty }
+let is_empty t = Option.is_none t.value && CharMap.is_empty t.children
 
 (* Создание словаря с одним элементом *)
 let singleton key value =
   let rec aux chars =
     match chars with
     | [] -> { value = Some value; children = CharMap.empty }
-    | c :: rest -> 
-      { value = None; children = CharMap.singleton c (aux rest) }
+    | c :: rest -> { value = None; children = CharMap.singleton c (aux rest) }
   in
   aux (List.of_seq (String.to_seq key))
 
@@ -35,12 +26,12 @@ let insert key value t =
     match chars with
     | [] -> { node with value = Some value }
     | c :: rest ->
-      let child = 
-        match CharMap.find_opt c node.children with
-        | Some child -> aux rest child
-        | None -> aux rest empty
-      in
-      { node with children = CharMap.add c child node.children }
+        let child =
+          match CharMap.find_opt c node.children with
+          | Some child -> aux rest child
+          | None -> aux rest empty
+        in
+        { node with children = CharMap.add c child node.children }
   in
   aux (List.of_seq (String.to_seq key)) t
 
@@ -49,36 +40,37 @@ let find_opt key t =
   let rec aux chars node =
     match chars with
     | [] -> node.value
-    | c :: rest ->
-      match CharMap.find_opt c node.children with
-      | Some child -> aux rest child
-      | None -> None
+    | c :: rest -> (
+        match CharMap.find_opt c node.children with
+        | Some child -> aux rest child
+        | None -> None)
   in
   aux (List.of_seq (String.to_seq key)) t
 
 (* Проверка наличия ключа *)
-let mem key t =
-  Option.is_some (find_opt key t)
+let mem key t = Option.is_some (find_opt key t)
 
 (* Удаление элемента. Очищает пустые ветви *)
 let remove key t =
   let rec aux chars node =
     match chars with
-    | [] -> 
-      let new_node = { node with value = None } in
-      if CharMap.is_empty new_node.children then None
-      else Some new_node
-    | c :: rest ->
-      match CharMap.find_opt c node.children with
-      | None -> Some node
-      | Some child ->
-        match aux rest child with
-        | None -> 
-          let new_children = CharMap.remove c node.children in
-          if Option.is_none node.value && CharMap.is_empty new_children then None
-          else Some { node with children = new_children }
-        | Some new_child ->
-          Some { node with children = CharMap.add c new_child node.children }
+    | [] ->
+        let new_node = { node with value = None } in
+        if CharMap.is_empty new_node.children then None else Some new_node
+    | c :: rest -> (
+        match CharMap.find_opt c node.children with
+        | None -> Some node
+        | Some child -> (
+            match aux rest child with
+            | None ->
+                let new_children = CharMap.remove c node.children in
+                if Option.is_none node.value && CharMap.is_empty new_children
+                then None
+                else Some { node with children = new_children }
+            | Some new_child ->
+                Some
+                  { node with children = CharMap.add c new_child node.children }
+            ))
   in
   match aux (List.of_seq (String.to_seq key)) t with
   | None -> empty
@@ -87,14 +79,12 @@ let remove key t =
 (* Преобразование в список пар (ключ, значение) в лексикографическом порядке *)
 let to_list t =
   let rec aux prefix node acc =
-    let acc = 
-      match node.value with
-      | Some v -> (prefix, v) :: acc
-      | None -> acc
+    let acc =
+      match node.value with Some v -> (prefix, v) :: acc | None -> acc
     in
-    CharMap.fold (fun c child acc ->
-      aux (prefix ^ String.make 1 c) child acc
-    ) node.children acc
+    CharMap.fold
+      (fun c child acc -> aux (prefix ^ String.make 1 c) child acc)
+      node.children acc
   in
   List.rev (aux "" t [])
 
@@ -125,9 +115,10 @@ let mapi f t =
   let rec aux prefix node =
     {
       value = Option.map (f prefix) node.value;
-      children = CharMap.mapi (fun c child -> 
-        aux (prefix ^ String.make 1 c) child
-      ) node.children;
+      children =
+        CharMap.mapi
+          (fun c child -> aux (prefix ^ String.make 1 c) child)
+          node.children;
     }
   in
   aux "" t
@@ -135,77 +126,66 @@ let mapi f t =
 (* Фильтрация по предикату *)
 let filter pred t =
   let rec aux prefix node =
-    let new_value = 
-      match node.value with
-      | Some v when pred prefix v -> Some v
-      | _ -> None
+    let new_value =
+      match node.value with Some v when pred prefix v -> Some v | _ -> None
     in
-    let new_children = 
-      CharMap.fold (fun c child acc ->
-        let new_prefix = prefix ^ String.make 1 c in
-        let new_child = aux new_prefix child in
-        if is_empty new_child then acc
-        else CharMap.add c new_child acc
-      ) node.children CharMap.empty
+    let new_children =
+      CharMap.fold
+        (fun c child acc ->
+          let new_prefix = prefix ^ String.make 1 c in
+          let new_child = aux new_prefix child in
+          if is_empty new_child then acc else CharMap.add c new_child acc)
+        node.children CharMap.empty
     in
     { value = new_value; children = new_children }
   in
   aux "" t
 
 (* Фильтрация только по значению *)
-let filter_values pred t =
-  filter (fun _ v -> pred v) t
+let filter_values pred t = filter (fun _ v -> pred v) t
 
 (* Левая свёртка в лексикографическом порядке ключей *)
 let fold_left f init t =
   let rec aux prefix acc node =
-    let acc = 
-      match node.value with
-      | Some v -> f acc prefix v
-      | None -> acc
-    in
-    CharMap.fold (fun c child acc ->
-      aux (prefix ^ String.make 1 c) acc child
-    ) node.children acc
+    let acc = match node.value with Some v -> f acc prefix v | None -> acc in
+    CharMap.fold
+      (fun c child acc -> aux (prefix ^ String.make 1 c) acc child)
+      node.children acc
   in
   aux "" init t
 
 (* Правая свёртка в обратном лексикографическом порядке *)
 let fold_right f t init =
   let rec aux prefix node acc =
-    let acc = 
-      CharMap.to_rev_seq node.children 
-      |> Seq.fold_left (fun acc (c, child) ->
-        aux (prefix ^ String.make 1 c) child acc
-      ) acc
+    let acc =
+      CharMap.to_rev_seq node.children
+      |> Seq.fold_left
+           (fun acc (c, child) -> aux (prefix ^ String.make 1 c) child acc)
+           acc
     in
-    match node.value with
-    | Some v -> f prefix v acc
-    | None -> acc
+    match node.value with Some v -> f prefix v acc | None -> acc
   in
   aux "" t init
 
 (* Итерация по всем парам *)
-let iter f t =
-  fold_left (fun () k v -> f k v) () t
+let iter f t = fold_left (fun () k v -> f k v) () t
 
 (* Объединение двух словарей - операция моноида.
    При конфликте ключей побеждает значение из второго словаря *)
 let merge t1 t2 =
   let rec aux n1 n2 =
-    let new_value = 
-      match n2.value with
-      | Some _ -> n2.value
-      | None -> n1.value
+    let new_value =
+      match n2.value with Some _ -> n2.value | None -> n1.value
     in
     let new_children =
-      CharMap.merge (fun _ c1 c2 ->
-        match c1, c2 with
-        | None, None -> None
-        | Some c, None -> Some c
-        | None, Some c -> Some c
-        | Some c1, Some c2 -> Some (aux c1 c2)
-      ) n1.children n2.children
+      CharMap.merge
+        (fun _ c1 c2 ->
+          match (c1, c2) with
+          | None, None -> None
+          | Some c, None -> Some c
+          | None, Some c -> Some c
+          | Some c1, Some c2 -> Some (aux c1 c2))
+        n1.children n2.children
     in
     { value = new_value; children = new_children }
   in
@@ -214,36 +194,36 @@ let merge t1 t2 =
 (* Объединение с пользовательской функцией разрешения конфликтов *)
 let merge_with f t1 t2 =
   let rec aux n1 n2 =
-    let new_value = 
-      match n1.value, n2.value with
+    let new_value =
+      match (n1.value, n2.value) with
       | None, None -> None
       | Some v, None -> Some v
       | None, Some v -> Some v
       | Some v1, Some v2 -> Some (f v1 v2)
     in
     let new_children =
-      CharMap.merge (fun _ c1 c2 ->
-        match c1, c2 with
-        | None, None -> None
-        | Some c, None -> Some c
-        | None, Some c -> Some c
-        | Some c1, Some c2 -> Some (aux c1 c2)
-      ) n1.children n2.children
+      CharMap.merge
+        (fun _ c1 c2 ->
+          match (c1, c2) with
+          | None, None -> None
+          | Some c, None -> Some c
+          | None, Some c -> Some c
+          | Some c1, Some c2 -> Some (aux c1 c2))
+        n1.children n2.children
     in
     { value = new_value; children = new_children }
   in
   aux t1 t2
 
 (* Конкатенация списка словарей *)
-let concat dicts =
-  List.fold_left merge empty dicts
+let concat dicts = List.fold_left merge empty dicts
 
 (* Эффективное сравнение без преобразования в списки.
    Сравнивает структуры напрямую рекурсивно *)
 let equal value_eq t1 t2 =
   let rec aux n1 n2 =
-    let values_eq = 
-      match n1.value, n2.value with
+    let values_eq =
+      match (n1.value, n2.value) with
       | None, None -> true
       | Some v1, Some v2 -> value_eq v1 v2
       | _ -> false
@@ -254,47 +234,46 @@ let equal value_eq t1 t2 =
       let keys2 = CharMap.bindings n2.children |> List.map fst in
       if keys1 <> keys2 then false
       else
-        CharMap.for_all (fun c child1 ->
-          match CharMap.find_opt c n2.children with
-          | None -> false
-          | Some child2 -> aux child1 child2
-        ) n1.children
+        CharMap.for_all
+          (fun c child1 ->
+            match CharMap.find_opt c n2.children with
+            | None -> false
+            | Some child2 -> aux child1 child2)
+          n1.children
   in
   aux t1 t2
 
 (* Получение всех ключей *)
-let keys t =
-  fold_left (fun acc k _ -> k :: acc) [] t |> List.rev
+let keys t = fold_left (fun acc k _ -> k :: acc) [] t |> List.rev
 
 (* Получение всех значений *)
-let values t =
-  fold_left (fun acc _ v -> v :: acc) [] t |> List.rev
+let values t = fold_left (fun acc _ v -> v :: acc) [] t |> List.rev
 
 (* Поиск всех пар с данным префиксом ключа *)
 let find_prefix prefix t =
   let rec navigate chars node =
     match chars with
     | [] -> Some node
-    | c :: rest ->
-      match CharMap.find_opt c node.children with
-      | Some child -> navigate rest child
-      | None -> None
+    | c :: rest -> (
+        match CharMap.find_opt c node.children with
+        | Some child -> navigate rest child
+        | None -> None)
   in
   match navigate (List.of_seq (String.to_seq prefix)) t with
   | None -> []
   | Some subtree ->
-    let rec collect current_prefix node acc =
-      let acc = 
-        match node.value with
-        | Some v -> (prefix ^ current_prefix, v) :: acc
-        | None -> acc
+      let rec collect current_prefix node acc =
+        let acc =
+          match node.value with
+          | Some v -> (prefix ^ current_prefix, v) :: acc
+          | None -> acc
+        in
+        CharMap.fold
+          (fun c child acc ->
+            collect (current_prefix ^ String.make 1 c) child acc)
+          node.children acc
       in
-      CharMap.fold (fun c child acc ->
-        collect (current_prefix ^ String.make 1 c) child acc
-      ) node.children acc
-    in
-    List.rev (collect "" subtree [])
+      List.rev (collect "" subtree [])
 
 (* Проверка наличия ключей с данным префиксом *)
-let has_prefix prefix t =
-  find_prefix prefix t <> []
+let has_prefix prefix t = find_prefix prefix t <> []
--- test/test_prefix_tree_dict.ml
+++ test/test_prefix_tree_dict.ml
@@ -17,10 +17,8 @@ let test_singleton _ =
   assert_equal None (find_opt "hell" d)
 
 let test_insert_find _ =
-  let d = empty 
-    |> insert "apple" 1
-    |> insert "app" 2
-    |> insert "banana" 3
+  let d =
+    empty |> insert "apple" 1 |> insert "app" 2 |> insert "banana" 3
     |> insert "band" 4
   in
   assert_equal (Some 1) (find_opt "apple" d);
@@ -41,10 +39,8 @@ let test_mem _ =
   assert_bool "not mem baz" (not (mem "baz" d))
 
 let test_remove _ =
-  let d = empty 
-    |> insert "apple" 1
-    |> insert "app" 2
-    |> insert "application" 3
+  let d =
+    empty |> insert "apple" 1 |> insert "app" 2 |> insert "application" 3
   in
   let d1 = remove "app" d in
   assert_equal None (find_opt "app" d1);
@@ -64,10 +60,10 @@ let test_empty_key _ =
   assert_equal 2 (size d)
 
 let test_list_conversion _ =
-  let pairs = [("apple", 1); ("app", 2); ("banana", 3)] in
+  let pairs = [ ("apple", 1); ("app", 2); ("banana", 3) ] in
   let d = of_list pairs in
   let result = to_list d in
-  assert_equal [("app", 2); ("apple", 1); ("banana", 3)] result
+  assert_equal [ ("app", 2); ("apple", 1); ("banana", 3) ] result
 
 let test_map _ =
   let d = empty |> insert "a" 1 |> insert "b" 2 |> insert "c" 3 in
@@ -83,10 +79,8 @@ let test_mapi _ =
   assert_equal (Some 4) (find_opt "cd" d')
 
 let test_filter _ =
-  let d = empty 
-    |> insert "apple" 1
-    |> insert "apricot" 2
-    |> insert "banana" 3
+  let d =
+    empty |> insert "apple" 1 |> insert "apricot" 2 |> insert "banana" 3
   in
   let d' = filter (fun k _ -> String.length k > 5) d in
   assert_equal None (find_opt "apple" d');
@@ -104,14 +98,14 @@ let test_fold_left _ =
   let sum = fold_left (fun acc _ v -> acc + v) 0 d in
   assert_equal 6 sum;
   let keys_list = fold_left (fun acc k _ -> k :: acc) [] d in
-  assert_equal ["c"; "b"; "a"] keys_list
+  assert_equal [ "c"; "b"; "a" ] keys_list
 
 let test_fold_right _ =
   let d = empty |> insert "a" 1 |> insert "b" 2 |> insert "c" 3 in
   let sum = fold_right (fun _ v acc -> acc + v) d 0 in
   assert_equal 6 sum;
   let keys_list = fold_right (fun k _ acc -> k :: acc) d [] in
-  assert_equal ["a"; "b"; "c"] keys_list
+  assert_equal [ "a"; "b"; "c" ] keys_list
 
 let test_merge _ =
   let d1 = empty |> insert "a" 1 |> insert "b" 2 in
@@ -125,14 +119,14 @@ let test_merge _ =
 let test_merge_with _ =
   let d1 = empty |> insert "a" 1 |> insert "b" 2 in
   let d2 = empty |> insert "b" 20 |> insert "c" 3 in
-  let d = merge_with (+) d1 d2 in
+  let d = merge_with ( + ) d1 d2 in
   assert_equal (Some 22) (find_opt "b" d)
 
 let test_concat _ =
   let d1 = singleton "a" 1 in
   let d2 = singleton "b" 2 in
   let d3 = singleton "c" 3 in
-  let d = concat [d1; d2; d3] in
+  let d = concat [ d1; d2; d3 ] in
   assert_equal 3 (size d)
 
 (* Тест равенства при разном порядке вставки *)
@@ -141,30 +135,32 @@ let test_equal _ =
   let d2 = empty |> insert "b" 2 |> insert "a" 1 in
   let d3 = empty |> insert "a" 1 |> insert "b" 3 in
   let d4 = empty |> insert "a" 1 in
-  assert_bool "одинаковые элементы в разном порядке" (equal (=) d1 d2);
-  assert_bool "разные значения" (not (equal (=) d1 d3));
-  assert_bool "разный размер" (not (equal (=) d1 d4))
+  assert_bool "одинаковые элементы в разном порядке" (equal ( = ) d1 d2);
+  assert_bool "разные значения" (not (equal ( = ) d1 d3));
+  assert_bool "разный размер" (not (equal ( = ) d1 d4))
 
 (* Дополнительный тест: много элементов в разном порядке *)
 let test_equal_many_elements _ =
-  let pairs = [("apple", 1); ("app", 2); ("banana", 3); ("band", 4); ("cat", 5)] in
+  let pairs =
+    [ ("apple", 1); ("app", 2); ("banana", 3); ("band", 4); ("cat", 5) ]
+  in
   let d1 = of_list pairs in
   let d2 = of_list (List.rev pairs) in
-  let d3 = of_list [("band", 4); ("app", 2); ("cat", 5); ("banana", 3); ("apple", 1)] in
-  assert_bool "равны при разном порядке 1" (equal (=) d1 d2);
-  assert_bool "равны при разном порядке 2" (equal (=) d1 d3);
-  assert_bool "равны при разном порядке 3" (equal (=) d2 d3)
+  let d3 =
+    of_list [ ("band", 4); ("app", 2); ("cat", 5); ("banana", 3); ("apple", 1) ]
+  in
+  assert_bool "равны при разном порядке 1" (equal ( = ) d1 d2);
+  assert_bool "равны при разном порядке 2" (equal ( = ) d1 d3);
+  assert_bool "равны при разном порядке 3" (equal ( = ) d2 d3)
 
 let test_keys_values _ =
   let d = empty |> insert "b" 2 |> insert "a" 1 |> insert "c" 3 in
-  assert_equal ["a"; "b"; "c"] (keys d);
-  assert_equal [1; 2; 3] (values d)
+  assert_equal [ "a"; "b"; "c" ] (keys d);
+  assert_equal [ 1; 2; 3 ] (values d)
 
 let test_find_prefix _ =
-  let d = empty 
-    |> insert "apple" 1
-    |> insert "application" 2
-    |> insert "apply" 3
+  let d =
+    empty |> insert "apple" 1 |> insert "application" 2 |> insert "apply" 3
     |> insert "banana" 4
   in
   let result = find_prefix "app" d in
@@ -181,31 +177,33 @@ let test_immutability _ =
   assert_equal 2 (size d2);
   assert_equal 0 (size d3)
 
-let unit_tests = "Unit тесты" >::: [
-  "empty" >:: test_empty;
-  "singleton" >:: test_singleton;
-  "insert_find" >:: test_insert_find;
-  "insert_overwrite" >:: test_insert_overwrite;
-  "mem" >:: test_mem;
-  "remove" >:: test_remove;
-  "remove_all" >:: test_remove_all;
-  "empty_key" >:: test_empty_key;
-  "list_conversion" >:: test_list_conversion;
-  "map" >:: test_map;
-  "mapi" >:: test_mapi;
-  "filter" >:: test_filter;
-  "filter_values" >:: test_filter_values;
-  "fold_left" >:: test_fold_left;
-  "fold_right" >:: test_fold_right;
-  "merge" >:: test_merge;
-  "merge_with" >:: test_merge_with;
-  "concat" >:: test_concat;
-  "equal" >:: test_equal;
-  "equal_many_elements" >:: test_equal_many_elements;
-  "keys_values" >:: test_keys_values;
-  "find_prefix" >:: test_find_prefix;
-  "immutability" >:: test_immutability;
-]
+let unit_tests =
+  "Unit тесты"
+  >::: [
+         "empty" >:: test_empty;
+         "singleton" >:: test_singleton;
+         "insert_find" >:: test_insert_find;
+         "insert_overwrite" >:: test_insert_overwrite;
+         "mem" >:: test_mem;
+         "remove" >:: test_remove;
+         "remove_all" >:: test_remove_all;
+         "empty_key" >:: test_empty_key;
+         "list_conversion" >:: test_list_conversion;
+         "map" >:: test_map;
+         "mapi" >:: test_mapi;
+         "filter" >:: test_filter;
+         "filter_values" >:: test_filter_values;
+         "fold_left" >:: test_fold_left;
+         "fold_right" >:: test_fold_right;
+         "merge" >:: test_merge;
+         "merge_with" >:: test_merge_with;
+         "concat" >:: test_concat;
+         "equal" >:: test_equal;
+         "equal_many_elements" >:: test_equal_many_elements;
+         "keys_values" >:: test_keys_values;
+         "find_prefix" >:: test_find_prefix;
+         "immutability" >:: test_immutability;
+       ]
 
 (* ===== PROPERTY-BASED ТЕСТЫ ===== *)
 
@@ -216,77 +214,84 @@ let key_gen = Q.Gen.(string_size ~gen:(char_range 'a' 'e') (int_range 0 5))
 let key_arb = Q.make ~print:(fun s -> "\"" ^ s ^ "\"") key_gen
 
 (* Генератор словаря *)
-let pairs_gen = Q.Gen.list_size (Q.Gen.int_range 0 20) (Q.Gen.pair key_gen Q.Gen.int)
+let pairs_gen =
+  Q.Gen.list_size (Q.Gen.int_range 0 20) (Q.Gen.pair key_gen Q.Gen.int)
+
 let dict_gen = Q.Gen.map PTD.of_list pairs_gen
-let dict_arb = Q.make 
-  ~print:(fun d -> PTD.to_list d |> List.map (fun (k,v) -> Printf.sprintf "(%s,%d)" k v) |> String.concat ";" |> Printf.sprintf "[%s]")
-  dict_gen
+
+let dict_arb =
+  Q.make
+    ~print:(fun d ->
+      PTD.to_list d
+      |> List.map (fun (k, v) -> Printf.sprintf "(%s,%d)" k v)
+      |> String.concat ";" |> Printf.sprintf "[%s]")
+    dict_gen
 
 (* Моноид: левый нейтральный элемент *)
 let prop_monoid_left_identity =
-  Q.Test.make ~name:"Моноид: merge empty d = d" ~count:100 dict_arb
-    (fun d -> PTD.equal Int.equal (PTD.merge PTD.empty d) d)
+  Q.Test.make ~name:"Моноид: merge empty d = d" ~count:100 dict_arb (fun d ->
+      PTD.equal Int.equal (PTD.merge PTD.empty d) d)
 
 (* Моноид: правый нейтральный элемент *)
 let prop_monoid_right_identity =
-  Q.Test.make ~name:"Моноид: merge d empty = d" ~count:100 dict_arb
-    (fun d -> PTD.equal Int.equal (PTD.merge d PTD.empty) d)
+  Q.Test.make ~name:"Моноид: merge d empty = d" ~count:100 dict_arb (fun d ->
+      PTD.equal Int.equal (PTD.merge d PTD.empty) d)
 
 (* Моноид: ассоциативность *)
 let prop_monoid_associativity =
   Q.Test.make ~name:"Моноид: ассоциативность merge" ~count:100
-    (Q.triple dict_arb dict_arb dict_arb)
-    (fun (a, b, c) -> 
-      PTD.equal Int.equal (PTD.merge (PTD.merge a b) c) (PTD.merge a (PTD.merge b c)))
+    (Q.triple dict_arb dict_arb dict_arb) (fun (a, b, c) ->
+      PTD.equal Int.equal
+        (PTD.merge (PTD.merge a b) c)
+        (PTD.merge a (PTD.merge b c)))
 
 (* Вставка затем поиск возвращает значение *)
 let prop_insert_find =
   Q.Test.make ~name:"insert k v; find k = Some v" ~count:200
-    (Q.pair key_arb Q.int)
-    (fun (k, v) ->
+    (Q.pair key_arb Q.int) (fun (k, v) ->
       let d = PTD.insert k v PTD.empty in
       PTD.find_opt k d = Some v)
 
 (* Удаление затем поиск возвращает None *)
 let prop_remove_find =
   Q.Test.make ~name:"remove k; find k = None" ~count:200
-    (Q.pair dict_arb key_arb)
-    (fun (d, k) ->
+    (Q.pair dict_arb key_arb) (fun (d, k) ->
       let d' = PTD.remove k d in
       PTD.find_opt k d' = None)
 
 (* size = длина to_list *)
 let prop_size_to_list =
-  Q.Test.make ~name:"size = length (to_list)" ~count:100 dict_arb
-    (fun d -> PTD.size d = List.length (PTD.to_list d))
+  Q.Test.make ~name:"size = length (to_list)" ~count:100 dict_arb (fun d ->
+      PTD.size d = List.length (PTD.to_list d))
 
 (* of_list (to_list d) = d *)
 let prop_of_list_to_list =
-  Q.Test.make ~name:"of_list (to_list d) = d" ~count:100 dict_arb
-    (fun d -> PTD.equal Int.equal (PTD.of_list (PTD.to_list d)) d)
+  Q.Test.make ~name:"of_list (to_list d) = d" ~count:100 dict_arb (fun d ->
+      PTD.equal Int.equal (PTD.of_list (PTD.to_list d)) d)
 
 (* map id = id *)
 let prop_map_identity =
-  Q.Test.make ~name:"map id = id" ~count:100 dict_arb
-    (fun d -> PTD.equal Int.equal (PTD.map (fun x -> x) d) d)
+  Q.Test.make ~name:"map id = id" ~count:100 dict_arb (fun d ->
+      PTD.equal Int.equal (PTD.map (fun x -> x) d) d)
 
 (* map (f . g) = map f . map g *)
 let prop_map_composition =
-  Q.Test.make ~name:"map (f . g) = map f . map g" ~count:100 dict_arb
-    (fun d ->
+  Q.Test.make ~name:"map (f . g) = map f . map g" ~count:100 dict_arb (fun d ->
       let f x = x + 1 in
       let g x = x * 2 in
-      PTD.equal Int.equal (PTD.map f (PTD.map g d)) (PTD.map (fun x -> f (g x)) d))
+      PTD.equal Int.equal
+        (PTD.map f (PTD.map g d))
+        (PTD.map (fun x -> f (g x)) d))
 
 (* filter true = id *)
 let prop_filter_true =
-  Q.Test.make ~name:"filter (const true) = id" ~count:100 dict_arb
-    (fun d -> PTD.equal Int.equal (PTD.filter (fun _ _ -> true) d) d)
+  Q.Test.make ~name:"filter (const true) = id" ~count:100 dict_arb (fun d ->
+      PTD.equal Int.equal (PTD.filter (fun _ _ -> true) d) d)
 
 (* filter false = empty *)
 let prop_filter_false =
-  Q.Test.make ~name:"filter (const false) = empty" ~count:100 dict_arb
-    (fun d -> PTD.equal Int.equal (PTD.filter (fun _ _ -> false) d) PTD.empty)
+  Q.Test.make ~name:"filter (const false) = empty" ~count:100 dict_arb (fun d ->
+      PTD.equal Int.equal (PTD.filter (fun _ _ -> false) d) PTD.empty)
 
 (* fold_left корректно суммирует *)
 let prop_fold_sum =
@@ -298,20 +303,18 @@ let prop_fold_sum =
 
 (* Рефлексивность равенства *)
 let prop_equal_reflexive =
-  Q.Test.make ~name:"equal рефлексивно: d = d" ~count:100 dict_arb
-    (fun d -> PTD.equal Int.equal d d)
+  Q.Test.make ~name:"equal рефлексивно: d = d" ~count:100 dict_arb (fun d ->
+      PTD.equal Int.equal d d)
 
 (* Симметричность равенства *)
 let prop_equal_symmetric =
-  Q.Test.make ~name:"equal симметрично" ~count:100
-    (Q.pair dict_arb dict_arb)
+  Q.Test.make ~name:"equal симметрично" ~count:100 (Q.pair dict_arb dict_arb)
     (fun (d1, d2) -> PTD.equal Int.equal d1 d2 = PTD.equal Int.equal d2 d1)
 
 (* Равенство не зависит от порядка вставки (для уникальных ключей) *)
 let prop_equal_order_independent =
   Q.Test.make ~name:"equal не зависит от порядка вставки" ~count:100
-    (Q.make pairs_gen)
-    (fun pairs ->
+    (Q.make pairs_gen) (fun pairs ->
       (* Убираем дубликаты, оставляя последнее значение для каждого ключа *)
       let dedup pairs =
         let tbl = Hashtbl.create 16 in
@@ -326,32 +329,31 @@ let prop_equal_order_independent =
 (* mem k = is_some (find_opt k) *)
 let prop_mem_find =
   Q.Test.make ~name:"mem k = is_some (find_opt k)" ~count:200
-    (Q.pair dict_arb key_arb)
-    (fun (d, k) -> PTD.mem k d = Option.is_some (PTD.find_opt k d))
+    (Q.pair dict_arb key_arb) (fun (d, k) ->
+      PTD.mem k d = Option.is_some (PTD.find_opt k d))
 
-let qcheck_tests = List.map QCheck_ounit.to_ounit2_test [
-  prop_monoid_left_identity;
-  prop_monoid_right_identity;
-  prop_monoid_associativity;
-  prop_insert_find;
-  prop_remove_find;
-  prop_size_to_list;
-  prop_of_list_to_list;
-  prop_map_identity;
-  prop_map_composition;
-  prop_filter_true;
-  prop_filter_false;
-  prop_fold_sum;
-  prop_equal_reflexive;
-  prop_equal_symmetric;
-  prop_equal_order_independent;
-  prop_mem_find;
-]
+let qcheck_tests =
+  List.map QCheck_ounit.to_ounit2_test
+    [
+      prop_monoid_left_identity;
+      prop_monoid_right_identity;
+      prop_monoid_associativity;
+      prop_insert_find;
+      prop_remove_find;
+      prop_size_to_list;
+      prop_of_list_to_list;
+      prop_map_identity;
+      prop_map_composition;
+      prop_filter_true;
+      prop_filter_false;
+      prop_fold_sum;
+      prop_equal_reflexive;
+      prop_equal_symmetric;
+      prop_equal_order_independent;
+      prop_mem_find;
+    ]
 
 let property_tests = "Property-based тесты" >::: qcheck_tests
 
-let () = 
-  run_test_tt_main ("Prefix Tree Dict тесты" >::: [
-    unit_tests;
-    property_tests;
-  ])
+let () =
+  run_test_tt_main ("Prefix Tree Dict тесты" >::: [ unit_tests; property_tests ])
